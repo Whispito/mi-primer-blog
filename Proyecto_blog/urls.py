082f93945eebcca36f2e46e3c4562f93243ac9ff
whispy <whispyes@gmail.com>
2018-07-13 00:57:38 +0200

    cambie el html de la pagina

--- Proyecto_blog/urls.py
+++ Proyecto_blog/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path
+from blog.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('',"home.html" ),
+    #re_path(r'^$',home ),
+    re_path(r'^$',post_list),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,"home.html")
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
